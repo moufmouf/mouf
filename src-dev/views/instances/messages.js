10f5759702f677b54343dbf90cf47d9d51ab4419
David Négrier <d.negrier@thecodingmachine.com>
2015-02-12 17:35:17 +0100

    removing UI related files

--- src-dev/views/instances/messages.js
+++
@@ -1,8 +0,0 @@
-
-function addMessage(msg, cssClass) {
-	if (jQuery("#messages").size() == 0) {
-		jQuery("<div/>").attr("id", "messages").prependTo(jQuery('#content'));
-	}
-	
-	jQuery("#messages").append("<div class='"+cssClass+"'>"+msg+"</div>");
-}
